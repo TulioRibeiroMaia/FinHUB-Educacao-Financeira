1e0897a3f97d88873dee15a5ac076117f346b77f
Mikaele <mikaelethainara@gmail.com>
2022-09-21 16:37:00 -0300

    Exibe livros inseridos

--- src/js/gerenciar_livros.js
+++ src/js/gerenciar_livros.js
@@ -1,5 +1,5 @@
 function criarLivro() {
-  var nome = document.getElementById("tituloLivro").value;
+  var titulo = document.getElementById("tituloLivro").value;
   var autora = document.getElementById("autoraLivro").value;
   var sinopse = document.getElementById("sinopseLivro").value;
   var imagem = document.getElementById("imagemLivro").value;
@@ -8,7 +8,7 @@ function criarLivro() {
   var dadosLivro = JSON.parse(localStorage.getItem('livro') || '[]');
   // Adiciona pessoa ao cadastro
   dadosLivro.push({
-      nome: nome,
+      titulo: titulo,
       autora: autora,
       sinopse: sinopse,
       imagem: imagem
@@ -16,9 +16,33 @@ function criarLivro() {
 
   // Salva a lista alterada
   localStorage.setItem("livro", JSON.stringify(dadosLivro));
+  // localStorage.clear();
 
   // console.log(dadosLivro);
   // PARA VISUALIZAR OS DADOS NO LOCALSTORAGE ACESSE AS FERRAMENTAS DO DESENVOLVEDOR (F12) NA ABA APLICAÇÃO, NO MENU LATERAL ESCOLHA ARMAZENAMENTO -> ARMAZENAMENTO LOCAL -> HTTP://...
-
 }
+
+function retornaDadoHTLM(dadosLivro){
+  let array = Object.values(dadosLivro);
+
+  var table = document.getElementById("bookTable");
+
+  for(i=0; i<array.length; i++){
+    var row = table.insertRow(i+1);
+    var titulo = row.insertCell(0);
+    var autora = row.insertCell(1);
+    var acoes = row.insertCell(2);
+
+    titulo.innerHTML = array[i].titulo;
+    autora.innerHTML = array[i].autora;
+    acoes.innerHTML = 'acao';
+  }
+}
+
+function main() {
+  var dadosLivro = JSON.parse(localStorage.getItem("livro"));
+  let a = retornaDadoHTLM(dadosLivro);
+}
+
+main();
 
